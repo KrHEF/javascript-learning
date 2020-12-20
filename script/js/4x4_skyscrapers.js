2de06937d125f7ef6da30f9c46612826a35c63a9
Fishov <pskovsmadman@mail.ru>
2020-12-20 13:30:56 +0300

    Find max numbers and full line by clues

--- script/js/4x4_skyscrapers.js
+++ script/js/4x4_skyscrapers.js
@@ -1,5 +1,5 @@
 function solvePuzzle (clues) {
-    if (!Grid.validate(clues.length)) { throw new Error('Wrong grid'); }
+    if ( !Grid.validateSize(clues.length) ) { throw new Error('Wrong grid size'); }
     
     const grid = new Grid(clues);
     grid.calculate();
@@ -11,191 +11,274 @@ function solvePuzzle (clues) {
   }
   
   
-  class Cell {
-    // static _numbers;
-    
-    constructor() {
-      this._number = 0;
-      this._numbersAvailable = new Set(Cell._numbers);
-    }
-    
-    get Number() { return this._number; }
-    set Number(number) { 
-      this._number = (number > 0 && number <= Cell.NumberMax) ? number : 0; 
-    }
-    
-    get HaveNumber() { 
-      return !!this._number 
-    }
-
-    get AvailableNumbersCount() {
-      return this._numbersAvailable.size;
-    }
-
-    get AvailableNumber() {
-      return (this._numbersAvailable.size === 1) ? this._numbersAvailable.values()[0] : 0;
-    }
+class Cell {
+  // static _numbers;
   
-    static get NumberMax() { 
-      return Cell._numbers[Cell._numbers.length - 1]; 
-    }
-  
-    static get NumbersCount() {
-      return Cell._numbers.length;
-    }
-    static set NumbersCount(number) { 
-      Cell._numbers = [];
-      for (let i = 1; i <= number; i++) {
-        Cell._numbers.push(i); 
-      } 
-      Cell._numbers.sort(); 
-    }
-  
-    removeAvailableNumber(number) {
-      this._numbersAvailable.delete(number);
-    }
-    
-    toString() {
-      return this.Number;
-    }
-  
+  constructor() {
+    this._number = 0;
+    this._availableNumbers = new Set(Cell._numbers);
   }
   
-  class Grid {
-    
-    constructor(clues) {
-      this._size = Math.floor(clues.length / 4);
-      this._clues = Grid._getCluesObjectBySize(clues, this._size);
-      this._rows = Grid._getEmptyCellsBySize(this._size);
-      this._cols = Grid._getTransposeGrid(this._rows);
+  get Number() { 
+    return this._number; 
+  }
+  set Number(number) { 
+    this._number = (number > 0 && number <= Cell.NumberMax) ? number : 0; 
+  }
+  
+  get HaveNumber() { 
+    return !!this._number 
+  }
 
-      this._numbersAvailableInCells = [];
-      for (let i = 1; i <= Cell.NumberMax; i++) {
-        this._numbersAvailableInCells[i] = new Set();
-          this._rows.forEach( (row) => row.forEach( (cell) => this._numbersAvailableInCells[i].add(cell) ) );
-      }
-    }  
+  get AvailableNumbersCount() {
+    return this._availableNumbers.size;
+  }
+
+  get AvailableNumber() {
+    return (this._availableNumbers.size === 1) ? this._availableNumbers.values()[0] : 0;
+  }
+
+  static get NumberMax() { 
+    return Cell._numbers[Cell._numbers.length - 1]; 
+  }
+
+  static get NumbersCount() {
+    return Cell._numbers.length;
+  }
+  static set NumbersCount(number) { 
+    Cell._numbers = [];
+    for (let i = 1; i <= number; i++) {
+      Cell._numbers.push(i); 
+    } 
+    Cell._numbers.sort(); 
+  }
+
+  removeAvailableNumber(number) {
+    this._availableNumbers.delete(number);
+  }
   
-    get HaveSolution() {
-      return this._rows.every( (row) => row.every( (cell) => cell.HaveNumber) );
+  toString() {
+    return this.Number;
+  }
+
+}
+  
+class CellInGrid extends Cell {
+
+  constructor(rowIndex, colIndex) {
+    super();
+    
+    this._restrictions = {}
+    this._rowIndex = rowIndex;
+    this._colIndex = colIndex;
+    this._curRowCells = [];
+    this._curColCells = [];
+  }
+
+  get RowIndex() {
+    return this._rowIndex;
+  }
+
+  get ColIndex() {
+    return this._colIndex;
+  }
+
+
+
+}
+
+class Grid {
+  
+  /**
+   * Constructor
+   * @param {number[]} clues 
+   */
+  constructor(clues) {
+    this._clues = clues;
+    this._size = Math.floor(clues.length / 4);
+    this._restrictions = {};
+    this._rows = [];
+    this._cols = [];
+    this._cells = [];
+    this._availableCellForNumbers = [];
+    this._failThread = false;
+
+    this._init();
+  }  
+
+  get HasSolution() {
+    return this._rows.every( (row) => row.every( (cell) => cell.HaveNumber) );
+  }
+  
+  static validateSize(length) {
+    return !(length % 4);
+  }
+
+  calculate() {
+    this._findMaxNumberNearBorder();
+    this._findNumberFromAvailable();
+    this._findFullLine();
+    this._findNumberFromAvailable();
+  }
+
+  _init() {
+      this._setCluesObjectBySize();
+      this._setEmptyCellsBySize();
+      this._setAvailableCellsForNumbers();
+  }
+
+  _setCluesObjectBySize() {
+    this._restrictions = {
+      up:    this._clues.slice(0, this._size),
+      right: this._clues.slice(this._size, this._size * 2),
+      down:  this._clues.slice(this._size * 2, this._size * 3).reverse(),
+      left:  this._clues.slice(this._size * 3, this._size * 4).reverse(),
+    }
+  }
+
+  _setEmptyCellsBySize() {
+    Cell.NumbersCount = this._size;
+
+    for (let row = 0; row < this._size; row++) {
+      this._rows[row] = [];
+      for (let col = 0; col < this._size; col++) {
+        const cell = new CellInGrid(row, col);
+        this._rows[row][col] = cell
+        this._cells.push(cell);
+      }
+    }
+
+    this._cols = this._rows.map((row, rowIndex) => row.map((cell, colIndex) => this._rows[colIndex][rowIndex]));
+  }
+
+  _setAvailableCellsForNumbers() {
+    for (let i = 1; i <= Cell.NumberMax; i++) {
+      this._availableCellForNumbers[i] = new Set();
+        this._rows.forEach( (row) => row.forEach( (cell) => this._availableCellForNumbers[i].add(cell) ) );
+    }
+  }
+
+  /**
+   * Поиск максимального числа около границы
+   * (когда в подсказке стоит 1)
+   */
+  _findMaxNumberNearBorder() {
+    const findMaxNumber = (clue, rowIndex, colIndex) => {
+      if (clue === 1) {
+        this._setNumberToCellByIndexes(Cell.NumberMax, rowIndex, colIndex);
+      }
     }
     
-    static validate(length) {
-      return !(length % 4);
-    }
-  
-    static _getCluesObjectBySize(clues, size) {
-      return  {
-        up: clues.slice(0, size),
-        right: clues.slice(size, size * 2),
-        down: clues.slice(size * 2, size * 3).reverse(),
-        left: clues.slice(size * 3, size * 4).reverse(),
+    this._restrictions.up.forEach( (clue, index) => findMaxNumber(clue, 0, index));
+    this._restrictions.right.forEach( (clue, index) => findMaxNumber(clue, index, -1));
+    this._restrictions.down.forEach( (clue, index) => findMaxNumber(clue, -1, index));
+    this._restrictions.left.forEach( (clue, index) => findMaxNumber(clue, index, 0));
+  }
+
+  /**
+   * Поиска полного ряда чисел
+   * (когда в подсказке стоит максимальное число )
+   */
+  _findFullLine() {
+    const findMaxClue = (clue, line) => {
+      if (clue === Cell.NumberMax) {
+        line.forEach( (cell, index) => this._setNumberToCell(index + 1, cell));
       }
     }
-  
-    static _getEmptyCellsBySize(size) {
-      Cell.NumbersCount = size;
-  
-      const cells = [];
-      for (let row = 0; row < size; row++) {
-        cells[row] = [];
-        for (let col = 0; col < size; col++) {
-          cells[row][col] = new Cell();
+    
+    this._restrictions.up.forEach( (clue, index)    => findMaxClue(clue, this._cols[index]));
+    this._restrictions.right.forEach( (clue, index) => findMaxClue(clue, this._rows[index]));
+    this._restrictions.down.forEach( (clue, index)  => findMaxClue(clue, this._cols[index]));
+    this._restrictions.left.forEach( (clue, index)  => findMaxClue(clue, this._rows[index]));
+  }
+
+  _findNumberFromAvailable() {
+    // let rowIndex = -1,
+        // colIndex = -1;
+      let findNumber = true;
+
+    while (findNumber) {
+      findNumber = false;
+
+      // Проверка на 1 доступное число
+      this._rows.forEach( (row, rowIndex) => row.forEach( (cell) => {
+        const number = cell.AvailableNumber;
+        if (number) {
+          findNumber = true;
+          this._setNumberToCellByIndexes(number, rowIndex, colIndex);
         }
-        
-      }
-          
-      return cells;
-    }
-    
-    static _getTransposeGrid(rows) { 
-      return rows.map((row, rowIndex) => row.map((cell, colIndex) => rows[colIndex][rowIndex]));
-    };
-    
-    calculate() {
-      this._findMaxNumberNearBorder();
-      this._findNumberFromAvailable();
-    }
-    
-    _findMaxNumberNearBorder() {
-      const findMaxNumber = (clue, rowIndex, colIndex) => {
-        if (clue === 1) {
-          this._setNumberToCell(Cell.NumberMax, rowIndex, colIndex);
-        }
-      }
-      
-      this._clues.up.forEach( (clue, index) => findMaxNumber(clue, 0, index));
-      this._clues.right.forEach( (clue, index) => findMaxNumber(clue, index, -1));
-      this._clues.down.forEach( (clue, index) => findMaxNumber(clue, -1, index));
-      this._clues.left.forEach( (clue, index) => findMaxNumber(clue, index, 0));
-    }
+      } ));
 
-    _findNumberFromAvailable() {
-      // let rowIndex = -1,
-          // colIndex = -1;
-        let findNumber = true;
-
-      while (findNumber) {
-        findNumber = false;
-
-        // Проверка на 1 доступное число
-        this._rows.forEach( (row, rowIndex) => row.forEach( (cell) => {
-          const number = cell.AvailableNumber;
-          if (number) {
+      // Проверка на 1 доступное место под число
+      this._availableCellForNumbers.forEach( (availableCells, number) => {
+        if (availableCells.size === 0) { return false; }
+        if (availableCells.size === 1) {
+          availableCells.forEach( (cell) => {
             findNumber = true;
-            this._setNumberToCell(number, rowIndex, colIndex);
-          }
-        } ));
-
-        // Проверка на 1 доступное место под число
-        this._numbersAvailableInCells.forEach( (numberCells, number) => {
-          // if (numberCells.size === 0)
-          if (numberCells.size === 1) {
-            // findNumber = true;
-            // const cell = numberCells.values()[0];
-            // this._setNumberToCell(number, rowIndex, colIndex);
-          }
-        });
-
-      }       
-    }
-  
-    _setNumberToCell(number, rowIndex, colIndex) {
-      rowIndex = (rowIndex < 0) ? Cell.NumbersCount + rowIndex : rowIndex;
-      colIndex = (colIndex < 0) ? Cell.NumbersCount + colIndex : colIndex;
-  
-      const cell = this._rows[rowIndex][colIndex];
-      cell.Number = number;
-      this._numbersAvailableInCells[number].delete(cell);
-      
-      this._removeAvailableNumberFromRow(rowIndex, number)
-      this._removeAvailableNumberFromColumn(colIndex, number)
-    }
-    
-    _removeAvailableNumberFromRow(index, number) {
-      const row = this._rows[index];
-      row.forEach( (cell) => {
-        cell.removeAvailableNumber(number);
-        this._numbersAvailableInCells[number].delete(cell);
+            this._setNumberToCellByIndexes(number, cell.RowIndex, cell.ColIndex);
+          });
+        }
       });
-    }
 
-    _removeAvailableNumberFromColumn(index, number) {
-      const col = this._cols[index];
-      col.forEach( (cell) => {
-        cell.removeAvailableNumber(number);
-        this._numbersAvailableInCells[number].delete(cell);
-      });
-    }
-
-    toString() { 
-      return this._rows.map( (row) => row.map( (cell) => cell.toString() ) ); 
-    }
-  
+    }       
   }
+
+  _setNumberToCellByIndexes(number, rowIndex, colIndex) {
+    rowIndex = (rowIndex < 0) ? Cell.NumbersCount + rowIndex : rowIndex;
+    colIndex = (colIndex < 0) ? Cell.NumbersCount + colIndex : colIndex;
+
+    const cell = this._rows[rowIndex][colIndex];
+
+    return this._setNumberToCell(number, cell);
+  }
+
+  _setNumberToCell(number, cell) {
+    if (cell.Number) {
+      return (cell.Number === number);
+    }
+     
+    cell.Number = number;
+    this._availableCellForNumbers[number].delete(cell);
+    
+    this._removeAvailableNumberFromRow(cell.RowIndex, number)
+    this._removeAvailableNumberFromColumn(cell.ColIndex, number)
+
+    return true;
+  }
+  
+  _removeAvailableNumberFromRow(index, number) {
+    const row = this._rows[index];
+    row.forEach( (cell) => {
+      cell.removeAvailableNumber(number);
+      this._availableCellForNumbers[number].delete(cell);
+    });
+  }
+
+  _removeAvailableNumberFromColumn(index, number) {
+    const col = this._cols[index];
+    col.forEach( (cell) => {
+      cell.removeAvailableNumber(number);
+      this._availableCellForNumbers[number].delete(cell);
+    });
+  }
+
+  _stopThread() {
+
+  }
+
+  _rollback() {
+
+  }
+
+  toString() { 
+    return this._rows.map( (row) => row.map( (cell) => cell.toString() ) ); 
+  }
+
+}
 
 
 const clues = [2, 2, 1, 3,
+// const clues = [2, 2, 1, 4,
         2, 2, 3, 1,
         1, 2, 2, 3,
         3, 2, 1, 3];
@@ -204,3 +287,4 @@ const expected = [[1, 3, 4, 2],
            [3, 4, 2, 1],
            [2, 1, 3, 4]];
 const actual = solvePuzzle(clues);
+console.log(actual);
