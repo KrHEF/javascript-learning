1913252c2b0ad430ae867024c0bc183b0da729c0
Evgenij Fishov <evgenij.fishov@softgamings.com>
2021-01-28 12:08:47 +0300

    Optimization

--- script/js/faberge.js
+++ script/js/faberge.js
@@ -9,15 +9,14 @@ function height(n,m) {
 
     console.time('Total');
     const { koef1, koef2, sum1 } = getKoef(n, m);
-    console.log(`(n,m) = (${n},${m}), koef1 = ${koef1.toString()}, koef2 = ${koef2.toString()}, sum1 = ${sum1.toString()}`);
+    // console.log(`(n,m) = (${n},${m}), koef1 = ${koef1.toString()}, koef2 = ${koef2.toString()}, sum1 = ${sum1.toString()}`);
     
-    console.time('sum');
     const sumResult = sumK1(koef1).plus( sumK2(koef2) );
-    console.timeEnd('sum');
 
-    const result = sum1.plus(sumResult);
-    console.log(`(n,m) = (${n},${m}), result = ${result.toString()}`);
+    const result = sumResult.plus(sum1);
     console.timeEnd('Total');
+    console.log(`(n,m) = (${n},${m}), result = ${result.toString()}`);
+    console.log('======================');
     return result;
 }
 
@@ -49,26 +48,38 @@ function  sumK2(koef) {
 }
 
 function getKoef(n, m) {
-  const koef1 = ( new Array(m - n) ).fill( new BigNumber(1) ), 
-        koef2 = ( new Array(n - 2) ).fill( new BigNumber(1) );
-  let sum1 = 0;  
+  const size = Math.max(m - n, n - 2),
+        count1 = n - 4,       // for (let i = 4; i <= n; i++) {...}
+        count2 = m - n - 2,   // for (let i = n + 2; i <= m; i++) {...}
+        countMin = (count1 < count2) ? count1 : count2,
+        countMax = (count1 < count2) ? count2 : count1;
 
-  console.time('koef1');
-  for (let i = 4; i <= n; i++) {
-    for (let j = 1; j < koef1.length; j++) {
-      koef1[j] = koef1[j].plus(koef1[j - 1]);
+  let sum1 = 0,
+      koef = ( new Array(size) ).fill( new BigNumber(1) ),
+      koef1 = new Array(m - n), 
+      koef2 = new Array(n - 2);
+  
+  console.time('koef');
+
+  for (let i = 0; i <= countMin; i++) {
+    for (let j = 1; j < koef.length; j++) {
+      koef[j] = koef[j].plus(koef[j - 1]);
     }
   }
-  console.timeEnd('koef1');
 
-  console.time('koef2');
-  for (let i = n + 2; i <= m; i++) {
-    for (let j = 1; j < koef2.length; j++) {
-      koef2[j] = koef2[j].plus(koef2[j - 1]);
+  koef1 = koef.slice(0, koef1.length);
+  koef2 = koef.slice(0, koef2.length);
+
+  koef = (count1 < count2) ? koef2 : koef1;
+
+  for (let i = countMin + 1; i <= countMax; i++) {
+    for (let j = 1; j < koef.length; j++) {
+      koef[j] = koef[j].plus(koef[j - 1]);
     }
   }
-  console.timeEnd('koef2');
- 
+
+  console.timeEnd('koef');
+
   sum1 = koef1.reduce( (sum, k) => sum.plus(k) ).minus(1);
 
   return { koef1, koef2: koef2.reverse(), sum1 };
@@ -89,7 +100,12 @@ height(477,500).toString();
 
 //16.7 s
 //6.77 s
+//4.90 s
 height(477,10000).toString();
 
+//91.9 s
+//62.5 s
 height(4477,10000).toString();
+
+//11.0 s
 height(9477,10000).toString();
