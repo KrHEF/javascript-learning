e7689d6f7975dfa0a55bbbad1abf96b0895d9936
Evgenij Fishov <evgenij.fishov@softgamings.com>
2021-03-24 14:56:58 +0300

    Minimize solution

--- script/js/words.js
+++ script/js/words.js
@@ -1,38 +1,21 @@
 function reverseLettersInWords(str) {
-    const result = ('' + str).split(' ').map((word) => {
+    const  isLetter = (char) => char.toLowerCase() !== char.toUpperCase(),
+        isUpperCase = (char) => char.toLowerCase() !== char;
+        
+    return ('' + str).split(' ').map((word) => {
         const noLetters = new Map(),
-            letterCase = new Map();
+            letterCases = new Map();
         
-        const reverseWord = word.split('').filter((char, index) => {
-            if (!isLetter(char)) {
-                noLetters.set(index, char);
-                return false;
-            }
-            letterCase.set(index, isUpperCase(char) );
-            return true;
-        }).reverse().map((char, index) => {
-            return letterCase.get(index) ? char.toUpperCase() : char.toLowerCase();
-        });
+        const reverseWord = word.split('').filter((char, index) => !isLetter(char) ? noLetters.set(index, char) && false : letterCases.set(index, isUpperCase(char) ) || true).reverse().map((char, index) => letterCases.get(index) ? char.toUpperCase() : char.toLowerCase());
+        noLetters.forEach((char, index) => reverseWord.splice(index, 0, char));
         
-
-        noLetters.forEach((char, index) => {
-            reverseWord.splice(index, 0, char);
-        });
         return reverseWord.join('');
     }).join(' ');
-    return result;
-}
-
-function isLetter(char) {
-    return char.toLowerCase() !== char.toUpperCase();
-}
-
-function isUpperCase(char) {
-    return char.toLowerCase() !== char;
 }
 
 
 
 console.log(reverseLettersInWords('Abc def, ght.') === 'Cba fed, thg.');
-console.log(reverseLettersInWords('Abc de,f ght.') === 'Cba fe,d thg.');
+console.log(reverseLettersInWords('Abc! de,f ght.') === 'Cba! fe,d thg.');
+console.log(reverseLettersInWords('Abc! .!123 de,f ght.') === 'Cba! .!123 fe,d thg.');
 console.log(reverseLettersInWords('') === '');
